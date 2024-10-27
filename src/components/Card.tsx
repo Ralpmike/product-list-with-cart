d4f9e67c2bbc25d757dfce82f5b0f6fb53bfb739
Ralpmike <ralphmike996@gmail.com>
2024-10-27 20:55:27 +0100

    update:

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -151,7 +151,7 @@ function ResponsiveImage({ image }: ImageProps) {
       <source media="(min-width: 1024px)" srcSet={image.desktop} />
       <source media="(min-width: 768px)" srcSet={image.tablet} />
       <source media="(min-width: 480px)" srcSet={image.mobile} />
-      <img src={image.thumbnail} alt="Product Image" />
+      <img src={image.mobile} alt="Product Image" />
     </picture>
   )
 }
